a3caef8ca6fa8c182bdcd5ac496f6a1568a2efc2
Josh Lanier <jlanier7@ivytech.edu>
2022-11-19 15:00:05 -0500

    Adding M04_ProgAssign_11.2.ipynb, zoo.py

---
+++ zoo.py
@@ -0,0 +1,57 @@
+"""
+------------------------------------------------------------------------------------------------------------------------------------
+
+ Program Name: M03 Lab - Car Profiler
+
+ GitHub Filepath: jlanier7/M02_SDEV220/M03_Lab.py
+
+ Author: Josh Lanier 
+
+ SDEV 220
+
+ 11/13/22
+
+ Program Purpose: 
+  
+1. Constants and variables:     
+INITIALIZED:  
+    - vehTypeAcceptable (tuple)     tuple with the acceptable value for vehType variable assignment input
+    - doorsAcceptable   (tuple)     tuple with the acceptable values for doors variable assignment input
+    - roofAcceptable    (tuple)     tuple with the acceptable values for roof variable assignment input
+IN-LINE:
+    - vehType           (string)    variable that holds input value for type of vehicle, to eventually pass to Car() as argument
+    - year              (string)    variable that holds input value for year of vehicle, to eventually pass to Car() as argument
+    - make              (string)    variable that holds input value for make of vehicle, to eventually pass to Car() as argument
+    - model             (string)    variable that holds input value for model of vehicle, to eventually pass to Car() as argument    
+    - doors             (string)    variable that holds input value for number of doors on the vehicle, to eventually pass to Car() as argument
+    - roof              (string)    variable that holds input value for type of roof on vehicle, to eventually pass to Car() as argument
+    - auto              (object)    object of class Car, created by variables gathering input in the while loop, that pass as attribute arguments at the end of the while 
+                                    loop in the program
+  
+2. Inputs
+
+- vehicle type          (string)
+- vehicle year          (string)
+- vehicle make          (string)
+- vehicle model         (string)
+- veh. number of doors  (string)
+- vehicle roof type     (string)
+
+3. Computations (pseudocode)
+
+not included for this assignment
+
+4. Outputs   
+
+not included for this assignment
+
+------------------------------------------------------------------------------------------------------------------------------------
+"""
+# importing libraries
+
+
+# initializing variables
+
+def hours():
+    '''Prints the hours of the zoo when function is called.'''
+    print("Open 9-5 daily")
